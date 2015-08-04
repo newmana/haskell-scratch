94b5cbdaee9c8a01443bb654f5bab35a1a876428
Andrew Newman <andrewfnewman@gmail.com>
2015-08-05 06:55:22 +1000

    Prelude either maybe

---
+++ MaybeEither.hs
@@ -0,0 +1,11 @@
+justA = Just "a"
+Just (a) = justA
+maybeToA = maybe "" id justA
+isA = a == "a"
+isAToo = maybeToA == "a"
+
+eitherC = Right "c"
+eitherD = Left "d"
+isC = either id id eitherC == "c"
+isD = either id id eitherD == "d"
+
