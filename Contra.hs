4ca27dc460127f057d688e9beb8c25b329660c56
Andrew Newman <andrewfnewman@gmail.com>
2017-04-29 17:10:55 +1000

    ?

---
+++ Contra.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE UnicodeSyntax #-}
+import           Control.Applicative
+import           Data.Functor.Contravariant (Contravariant (..))
+-- show
+newtype Predicate a = Predicate { getPredicate ∷ a → Bool }
+
+instance Contravariant Predicate where
+    contramap g (Predicate p) = Predicate (p . g)
+
+veryOdd ∷ Predicate Integer
+veryOdd = contramap (`div` 2) (Predicate odd)
+
+main ∷ IO ()
+main = print $ getPredicate veryOdd <$> [0 .. 11]
