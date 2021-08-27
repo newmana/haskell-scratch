e34095a937d62dd21b1edc7633f731515c1bff56
Andrew Newman <andrewfnewman@gmail.com>
2021-08-28 05:40:03 +1000

    Fix constants

--- src/Lib/Planets.hs
+++ src/Lib/Planets.hs
@@ -4,20 +4,19 @@ module Lib.Planets where
 import Control.Monad
 import System.Environment   
 
-data Planet = Mercury | Venus | Earth | Mars | Jupiter | Saturn | Uranus | Neptune | Pluto deriving (Enum, Show, Eq)
+data Planet = Mercury | Venus | Earth | Mars | Jupiter | Saturn | Uranus | Neptune deriving (Enum, Show, Eq)
 
 data PlanetStat = PlanetStat { planet :: Planet, mass :: Double, radius :: Double }
 
 stat :: Planet -> PlanetStat
-stat Mercury = PlanetStat { planet = Mercury, mass = 3.303e+23, radius = 2.4397e6  }
-stat Venus   = PlanetStat { planet = Venus,   mass = 4.869e+24, radius = 6.0518e6  }
-stat Earth   = PlanetStat { planet = Earth,   mass = 5.976e+24, radius = 6.37814e6 }
-stat Mars    = PlanetStat { planet = Mars,    mass = 6.421e+23, radius = 3.3972e6  }
-stat Jupiter = PlanetStat { planet = Jupiter, mass = 1.9e+27,   radius = 7.1492e7  }
-stat Saturn  = PlanetStat { planet = Saturn,  mass = 5.688e+26, radius = 6.0268e7  }
-stat Uranus  = PlanetStat { planet = Uranus,  mass = 8.686e+25, radius = 2.5559e7  }
-stat Neptune = PlanetStat { planet = Neptune, mass = 1.024e+26, radius = 2.4746e7  }
-stat Pluto   = PlanetStat { planet = Pluto,   mass = 1.27e+22,  radius = 1.137e6   }
+stat Mercury = PlanetStat { planet = Mercury, mass = 3.301e+23, radius = 2.4394e6  }
+stat Venus   = PlanetStat { planet = Venus,   mass = 4.867e+24, radius = 6.052e6   }
+stat Earth   = PlanetStat { planet = Earth,   mass = 5.972e+24, radius = 6.3710e6  }
+stat Mars    = PlanetStat { planet = Mars,    mass = 6.417e+23, radius = 3.3895e6  }
+stat Jupiter = PlanetStat { planet = Jupiter, mass = 1.898e+27, radius = 6.9911e7  }
+stat Saturn  = PlanetStat { planet = Saturn,  mass = 5.685e+26, radius = 5.8232e7  }
+stat Uranus  = PlanetStat { planet = Uranus,  mass = 8.682e+25, radius = 2.362e7   }
+stat Neptune = PlanetStat { planet = Neptune, mass = 1.024e+26, radius = 2.4622e7  }
 
 g :: Double
 g = 6.67300E-11
